2d942480fd4b0ec5becef5d5f8034a86cf1c935a
zjs0614 <zjs0614@gmail.com>
2021-01-20 00:51:34 +0800

    min max depth

---
+++ src/p111_minimum-depth-of-binary-tree/sol.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left = self.minDepth(root.left)
+            right = self.minDepth(root.right)
+            if left == 0:
+                return 1 + right
+            elif right == 0:
+                return 1 + left
+            else:
+                return 1 + min(left, right)
