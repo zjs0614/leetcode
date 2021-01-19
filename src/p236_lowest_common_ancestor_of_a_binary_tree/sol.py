2c72aaa52403b5e9b23b9c38168f9fef8b7dec20
zjs0614 <zjs0614@gmail.com>
2021-01-19 22:55:18 +0800

    p236

---
+++ src/p236_lowest_common_ancestor_of_a_binary_tree/sol.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root.val == p.val or root.val == q.val:
+            return root
+        else:
+            left = self.lowestCommonAncestor(root.left, p, q)
+            right = self.lowestCommonAncestor(root.right, p, q)
+            return root if left is not None and right is not None else (left if left is not None else right)
+
