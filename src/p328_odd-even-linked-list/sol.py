1bfba46f59ff0eacd1a768b53327ccc83bdaeba6
zjs0614 <zjs0614@gmail.com>
2021-03-10 12:25:26 +0800

    p328

---
+++ src/p328_odd-even-linked-list/sol.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        odd, even, even_head = head, head.next, head.next
+        while odd.next is not None and odd.next.next is not None:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
