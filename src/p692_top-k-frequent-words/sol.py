bb7d018bfb1a197482c8b581148c67d784ea30e1
zjs0614 <zjs0614@gmail.com>
2021-03-22 02:29:58 +0800

    p692

---
+++ src/p692_top-k-frequent-words/sol.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count_map = {}
+        for word in words:
+            if word in count_map:
+                count_map[word] += 1
+            else:
+                count_map[word] = 1
+
+        return sorted(count_map, key=lambda x: (len(words)-count_map[x], x), reverse=False)[:k]
